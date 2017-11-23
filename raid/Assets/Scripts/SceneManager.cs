fcd0d6a8df91c1f5ce2b0ea0d9b7edb435b0ac26
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-11-23 14:42:43 +0200

    no message

--- raid/Assets/Scripts/SceneManager.cs
+++ raid/Assets/Scripts/SceneManager.cs
@@ -10,28 +10,55 @@ public class SceneManager : MonoBehaviour
 	public GameObject [] Enemies;
 
 	public float StartWait = 2;
-	public float spawnWait = 1;
+	public float spawnWait = 2;
 
 	bool hasInvoked = false;
+	int roundNbr = 0;
 
 	void Update()
 	{
 		Enemies = GameObject.FindGameObjectsWithTag ("Enemy");
 
-		if (Enemies.Length < amountToSpawn && hasInvoked == false)
+		if (Enemies.Length == 0 && hasInvoked == false)
 		{
+			RoundManager();
+		}
+	}
+
+	IEnumerator RoundManager ()
+	{
+		yield return new WaitForSeconds (1);
+
+		switch (roundNbr)
+		{
+		case 0: 
+			hasInvoked = true;
+			amountToSpawn = 2;
 			StartCoroutine (Spawn());
-			//Invoke ("Spawn", 1);
+			break;
+		case 1: 
+			hasInvoked = true;
+			amountToSpawn = 5;
+			StartCoroutine (Spawn());
+			break;
+		case 2: 
+			hasInvoked = true;
+			amountToSpawn = 8;
+			StartCoroutine(Spawn());
+			break;
 		}
 	}
 
 	IEnumerator Spawn()
 	{
-		hasInvoked = true;
-		yield return new WaitForSeconds (StartWait);
-		GameObject enemy = Instantiate (EnemyPrefabs[Random.Range(0, EnemyPrefabs.Length)], spawns[Random.Range(0, spawns.Length)]);
-		enemy.transform.parent = null;
+		while (Enemies.Length < amountToSpawn)
+		{
+			yield return new WaitForSeconds (StartWait);
+			GameObject enemy = Instantiate (EnemyPrefabs[Random.Range(0, EnemyPrefabs.Length)], spawns[Random.Range(0, spawns.Length)]);
+			enemy.transform.parent = null;
+		}
 		yield return new WaitForSeconds (spawnWait);
+		roundNbr += 1;
 		hasInvoked = false;
 	}
 }
