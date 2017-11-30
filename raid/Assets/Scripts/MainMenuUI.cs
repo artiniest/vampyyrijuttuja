820b0937f913b3ad818b28f0eeac508d02b2ce8b
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-11-30 10:36:30 +0200

    no message

---
+++ raid/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour 
+{
+	public void StartGame ()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Intro");
+	}
+
+	public void Tutorial()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Tutorial");
+	}
+}
