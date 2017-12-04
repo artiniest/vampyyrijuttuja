43cc3f381991b2410aa82f6735935d41ffdf9eac
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-12-04 14:52:57 +0200

    no message

--- raid/Assets/Scripts/Bat.cs
+++ raid/Assets/Scripts/Bat.cs
@@ -6,4 +6,25 @@ public class Bat : MonoBehaviour
 {
 	public GameObject bat;
 	public GameObject[] Points;
+	int nextPoint = 0;
+
+	bool spawned = false;
+
+	void Update ()
+	{
+		if (spawned == true)
+		{
+			bat.transform.position = Vector2.MoveTowards(bat.transform.position, Points[nextPoint].transform.position, 0.07f);
+
+			if (bat.transform.position == Points[nextPoint].transform.position && nextPoint < Points.Length -1)
+			{
+				nextPoint ++;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		spawned = true;
+	}
 }
